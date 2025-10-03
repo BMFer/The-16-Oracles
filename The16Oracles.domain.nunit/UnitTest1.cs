5907d1e581d4396e716d0f1b7f70f599f4a57220
Shadow Oracle <j.hertz@vertexwireless.com>
2025-10-03 13:58:24 -0500

    add domain unit testing

---
+++ The16Oracles.domain.nunit/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace The16Oracles.domain.nunit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
